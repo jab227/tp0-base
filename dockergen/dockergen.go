0a362235a35b0c71e5fe1fd7f23c929506a5e2a4
Juan <jaguirreb@fi.uba.ar>
2024-09-05 15:04:06 -0300

    removed enviroment

--- dockergen/dockergen.go
+++ dockergen/dockergen.go
@@ -46,14 +46,11 @@ func writeNetworkConfig(builder *strings.Builder) {
 
 func writeClient(builder *strings.Builder, id int) {
 	clientName := fmt.Sprintf("client%d", id)
-	clientIdEnv := fmt.Sprintf("CLI_ID=%d", id)
 
 	writeKeyValue(builder, 2, pair{key: clientName})
 	writeKeyValue(builder, 4, pair{key: "container_name", value: clientName})
 	writeKeyValue(builder, 4, pair{key: "image", value: "client:latest"})
 	writeKeyValue(builder, 4, pair{key: "entrypoint", value: "/client"})
-	writeKeyValue(builder, 4, pair{key: "environment"})
-	writeItemList(builder, 6, clientIdEnv, "CLI_LOG_LEVEL=INFO")
 	writeKeyValue(builder, 4, pair{key: "networks"})
 	writeItemList(builder, 6, "testing_net")
 	writeKeyValue(builder, 4, pair{key: "depends_on"})
@@ -90,8 +87,6 @@ func writeServer(builder *strings.Builder) {
 	writeKeyValue(builder, 4, pair{key: "container_name", value: "server"})
 	writeKeyValue(builder, 4, pair{key: "image", value: "server:latest"})
 	writeKeyValue(builder, 4, pair{key: "entrypoint", value: "python3 ./main.py"})
-	writeKeyValue(builder, 4, pair{key: "environment"})
-	writeItemList(builder, 6, "PYTHONUNBUFFERED=1", "LOGGING_LEVEL=INFO")
 	writeKeyValue(builder, 4, pair{key: "volumes"})
 	writeItemList(builder, 6, "./server:/echoserver")
 	writeKeyValue(builder, 4, pair{key: "networks"})		
