6c5e692d1b81cee98d78ee80ad1846053e74b823
Juan <jaguirreb@fi.uba.ar>
2024-09-05 16:37:34 -0300

    try different form of test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -11,7 +11,7 @@ fi
 MESSAGE="echo server: this should be the same"
 
 RECEIVED=$(printf "%s" ${MESSAGE} | nc 0.0.0.0 ${SERVER_PORT} | tr -d '\n')
-if [[ "${RECEIVED}" -ne "${MESSAGE}" ]]
+if [ "${RECEIVED}" -ne "${MESSAGE}" ]
 then
     echo "action: test_echo_server | result: fail"
     exit 1
