b19e2b55f357353ddcf149a776c17c95f11fa8b4
Juan <jaguirreb@fi.uba.ar>
2024-09-05 17:00:59 -0300

    the command has to be evaluated inside container

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -12,8 +12,8 @@ fi
 NETWORK="tp0_testing_net"
 MESSAGE="echo server: this should be the same"
 
-COMMAND=$(printf "%s" ${MESSAGE} | nc server ${SERVER_PORT})
-RECEIVED=$(docker run --rm --network ${NETWORK} alpine sh -c ${COMMAND})
+COMMAND="printf "%s" ${MESSAGE} | nc server ${SERVER_PORT}"
+RECEIVED=$(docker run --rm --network ${NETWORK} alpine sh -c $(${COMMAND}))
 
 if [ "${RECEIVED}" -ne "${MESSAGE}" ]
 then
