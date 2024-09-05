bb62cb5507a4a71274763f8e9244040e25e9c364
Juan <jaguirreb@fi.uba.ar>
2024-09-05 15:56:27 -0300

    send only one message

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,15 +8,14 @@ then
     SERVER_PORT=12345
 fi
 
-TEST_MESSAGES=("echo" "server" "this" "should" "be" "the" "same")
+MESSAGE="echo server: this should be the same"
 
-for m in ${TEST_MESSAGES[@]}; do
-    RECEIVED=$(printf "%s" "$m" | nc server ${SERVER_PORT} | tr -d '\n')
-    if [ "${RECEIVED}" != "$m" ]
-    then
-        echo "action: test_echo_server | result: fail"
-        exit 1
-    fi
-done
+RECEIVED=$(printf "%s" "${MESSAGE}" | nc server ${SERVER_PORT} | tr -d '\n')
+if [ "${RECEIVED}" != "$m" ]
+then
+    echo "action: test_echo_server | result: fail"
+    exit 1
+fi
+
 echo "action: test_echo_server | result: success"
 exit 0
