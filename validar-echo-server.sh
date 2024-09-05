1f0efe3aa8dd990b2f40fdb7787c665037de2812
Juan <jaguirreb@fi.uba.ar>
2024-09-05 16:14:41 -0300

    fixed typo

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -11,11 +11,10 @@ fi
 MESSAGE="echo server: this should be the same"
 
 RECEIVED=$(printf "%s" ${MESSAGE} | nc 0.0.0.0 ${SERVER_PORT} | tr -d '\n')
-if [ "${RECEIVED}" -ne "${MESSAGE}" ]
+if [[ "${RECEIVED}" -ne "${MESSAGE}" ]]
 then
     echo "action: test_echo_server | result: fail"
     exit 1
 fi
-
 echo "action: test_echo_server | result: success"
 exit 0
